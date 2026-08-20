bd2a6fa7396dda4873e86e0e1c123c97def0c5c5
RA341 <razerishi1505@gmail.com>
2026-08-20 22:51:23 +0530

    remove py downloader

--- downloader/readme.md
+++
@@ -1,6 +0,0 @@
-# Depot
-
-Swiss tool of downloader manager
-
-
-https://spankbang.com/4x77a-a3njby/playlist/softcore
--- downloader/utils/auth.py
+++
@@ -1,7 +0,0 @@
-from fastapi import Request
-
-
-async def require_auth(request: Request) -> None:
-    # TODO: Implement actual authentication logic
-    # For now, we'll just allow all requests or check for a dummy header
-    pass
